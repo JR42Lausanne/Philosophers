81b0c02067a6cf1bec9fa74d34913250d10804cf
Julien Laïti <julien.laiti@gmail.com>
2023-04-03 17:44:54 +0200

    add .h

---
+++ include/philo.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlaiti <jlaiti@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/03 15:57:05 by jlaiti            #+#    #+#             */
+/*   Updated: 2023/04/03 17:36:37 by jlaiti           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+typedef struct s_args
+{
+	int	nb_philo;
+	int	nb_forks;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	nb_of_loop_philo;
+
+}		t_args;
+
+void	manage_philo(int argc, char **argv);
+int	parse_args(int argc, char **argv, t_args *args);
+
+//utils
+
+void	ft_puchar_fd(char c, int fd);
+void	ft_putstr_fd(char *str, int fd);
+int	ft_atoi(char *str);
+
+#endif
