f79ad66c85515721e95ba23eac4d414746fcbb45
Julien Laïti <julien.laiti@gmail.com>
2023-04-24 19:52:29 +0200

    little modified

--- src/manage_philo.c
+++ src/manage_philo.c
@@ -6,7 +6,7 @@
 /*   By: jlaiti <jlaiti@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 14:03:27 by jlaiti            #+#    #+#             */
-/*   Updated: 2023/04/11 17:54:43 by jlaiti           ###   ########.fr       */
+/*   Updated: 2023/04/19 10:51:40 by jlaiti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,13 +45,13 @@ t_philo	*manage_philo(t_args	*args)
 	forks = init_forks(args);
 	if (!forks)
 		return (NULL);
+	philo->start_time = get_time();
 	while (i < args->nb_philo)
 	{
 		philo[i].id = i + 1;
 		philo[i].nb_of_eat = 0;
 		philo[i].time_to_eat = args->time_to_eat;
 		philo[i].time_to_sleep = args->time_to_sleep;
-		philo[i].start_time = get_time();
 		philo[i].left_fork = forks[i];
 		philo[i].right_fork = forks[(i + 1) % args->nb_philo];
 		i++;
