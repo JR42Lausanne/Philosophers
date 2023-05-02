c8dad3f48c7c021a04fe7348df76f6159b76af21
Julien Laiti <julien.laiti@gmail.com>
2023-05-02 11:57:09 +0200

    fix correct message if all philo are eat

--- src/print_message.c
+++ src/print_message.c
@@ -6,7 +6,7 @@
 /*   By: jlaiti <jlaiti@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 10:59:40 by jlaiti            #+#    #+#             */
-/*   Updated: 2023/05/02 11:27:59 by jlaiti           ###   ########.fr       */
+/*   Updated: 2023/05/02 11:54:26 by jlaiti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,20 @@
 
 void	print_msg(int time_ms, char *msg, t_philo *philo, t_table *table)
 {
-	(void) time_ms; //TODO remove time_ms from arguments
+	static int	should_stop;
+
+	(void) time_ms;
 	if (!check_is_alive(&table->stop, &table->mutex_stop))
 	{
 		if (pthread_mutex_lock(&table->write_mutex) == -1)
 			return ;
-		printf("%d: philosopher %d - %s", get_time(),
+		if (!should_stop && msg[0] != 'A')
+			printf("%d: philosopher %d - %s", get_time(),
 			philo->id, msg);
+		else if (msg[0] == 'A')
+			printf("%s\n", msg);
+		if (msg[0] == 'd' || msg[0] == 'A')
+			should_stop = 1;
 		if (pthread_mutex_unlock(&table->write_mutex) == -1)
 			return ;
 	}	
